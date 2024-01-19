7a2010345645fc107d48cb65dde8d8accd0c9d16
Lukaszgrodzki23 <115477342+Lukaszgrodzki23@users.noreply.github.com>
2024-01-19 23:43:51 +0100

    update

---
+++ projektsieci2/server/Server.h
@@ -0,0 +1,40 @@
+#ifndef TCPSERVER_HPP
+#define TCPSERVER_HPP
+
+#include <QObject>
+#include <QTcpServer>
+#include <QHash>
+#include <QTcpSocket>
+#include "Game_state.h"
+#include "_server.h"
+#include "myserverbridge.h"
+
+class TcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TcpServer(QObject *parent = nullptr, int numPlayers = 0);
+    int numberOfPlayers;
+
+signals:
+    void newMessage(const std::vector<bool> &ba);
+    void clientDisconnected(int i);
+
+private slots:
+    void onNewConnection();
+    void onReadyRead(int client);
+    void onClientDisconnected(int client);
+    void onNewMessage(const std::vector<bool> &ba);
+
+
+private:
+    int numPlayers;
+    Game_state game_state;
+    QString getClientKey(const QTcpSocket *client) const;
+
+    Server _server;
+    QHash<int, int> _clients;
+
+};
+
+#endif
